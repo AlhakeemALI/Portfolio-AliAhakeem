9aff20e9976773af4cba4c9a0dc6247e0482b800
Ali <90877977+AlhakeemALI@users.noreply.github.com>
2023-03-21 20:43:29 -0600

    Adding Social Media Icons

---
+++ client/src/ProfileContainer/Home/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+      <div className='profile-container'>
+      <div className='profile-parent'>
+          <div className='profile-details'>
+             <div className='colz'>
+                     <a href='https://www.linkedin.com/in/ali-al-hakeem/'>
+                           <i className='fa fa-linkedin-square'>
+
+                           </i>
+                     </a>
+                     <a href='https://github.com/AlhakeemALI'>
+                           <i className='fa fa-github-square'>
+                           </i>
+                     </a>
+             </div>
+          </div>
+      </div>
+
+</div>
+  )
+}
+
+export default Profile
+
+
+
+
+
+
+
